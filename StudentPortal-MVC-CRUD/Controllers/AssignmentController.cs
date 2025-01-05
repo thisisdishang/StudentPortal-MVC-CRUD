107d4302923361e317b6329460dfb2a0bc121766
thisisdishang <dishangkumarrana@gmail.com>
2025-01-05 18:45:45 +0530

    Update assignment controller and change the view

--- StudentPortal-MVC-CRUD/Controllers/AssignmentController.cs
+++ StudentPortal-MVC-CRUD/Controllers/AssignmentController.cs
@@ -29,13 +29,13 @@ namespace StudentPortal_MVC_CRUD.Controllers
                 AssignmentNumber = viewModel.AssignmentNumber,
                 AssignmentSem = viewModel.AssignmentSem,
                 AssignmentContent = viewModel.AssignmentContent,
-                Subject=viewModel.Subject
+                Subject = viewModel.Subject
             };
 
             await dbContext.Assignment.AddAsync(assignment);
             await dbContext.SaveChangesAsync();
 
-            return RedirectToAction("List","Assignment");
+            return RedirectToAction("List", "Assignment");
 
         }
 
@@ -45,6 +45,32 @@ namespace StudentPortal_MVC_CRUD.Controllers
             var assignment = await dbContext.Assignment.ToListAsync();
             return View(assignment);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var assignment = await dbContext.Assignment.FindAsync(id);
+            return View(assignment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Assignment viewModel)
+        {
+            var assignment = await dbContext.Assignment.FindAsync(viewModel.AssignmentId);
+
+            if (assignment != null)
+            {
+                assignment.AssignmentNumber = viewModel.AssignmentNumber;
+                assignment.AssignmentSem = viewModel.AssignmentSem;
+                assignment.AssignmentContent = viewModel.AssignmentContent;
+                assignment.Subject = viewModel.Subject;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Assignment");
+        }
+
     }
 
 
